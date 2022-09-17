8fdb6c4a314b8d96481b504fceb06937b82e4033
anusuva200 <anusuvachaudhary@gmail.com>
2022-09-17 09:12:53 -0700

    check out contact us

---
+++ formhandler.php
@@ -0,0 +1,25 @@
+<?php
+$name=$_POST['email'];
+$visitor_email=$_POST['subject'];
+$message=$_POST['message'];
+
+$email_from='spam@gmail.com';
+
+$email_subject='New form submission';
+
+$email_body= "user Name: $name.\n". 
+"user email: $visitor_email.\n".  
+"Subject : $subject.\n".  
+"User Message: $message.\n".;  
+
+$to ='anusuva@gmail.com';
+
+$headers="From: $email_form \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n"
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact_us.html")
+?>
+
