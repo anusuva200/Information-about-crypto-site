0f02802fca6db5e5da7823bf0e167947d8aadb08
anusuva200 <anusuvachaudhary@gmail.com>
2022-09-21 05:28:59 -0700

    updated php file

--- formhandler.php
+++ formhandler.php
@@ -4,7 +4,7 @@ $visitor_email=$_POST['email'];
 $subject=$_POST['subject'];
 $message=$_POST['message'];
 
-$email_from='spam@gmail.com';
+$email_from='anusuvachaudhary@gmail.com';
 
 $email_subject='New form submission';
 
@@ -13,7 +13,7 @@ $email_body= "User Name: $name.\n".
 "Subject : $subject.\n".  
 "User Message: $message.\n".;  
 
-$to ='anusuva@gmail.com';
+$to ='anusuvachaudhary200@gmail.com';
 
 $headers="From: $email_form \r\n";
 
